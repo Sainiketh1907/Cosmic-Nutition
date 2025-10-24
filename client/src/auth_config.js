fcf3f358e3a55aaaa0177eb2ce9f0e9617936e02
Sainiketh1907 <bandari.24bcs10359@sst.scaler.com>
2025-10-24 17:24:24 +0530

    commit

--- client/src/auth_config.js
+++ client/src/auth_config.js
@@ -1,4 +1,4 @@
 // src/auth_config.js
-export const domain = "dev-wtx6rsim0yowfr3y.us.auth0.com"; // Replace with your Auth0 domain
-export const clientId = "qfGe72uE0LL5KVzkAGFCTHjFvja3GlxQ"; // Replace with your Auth0 client ID
-export const audience = "https://api.nutrition-tracker.com"; // Replace with your Auth0 API Identifier (Audience)// The issuer base URL, derived from your domain
+export const domain = "dev-wtx6rsim0yowfr3y.us.auth0.com";
+export const clientId = "qfGe72uE0LL5KVzkAGFCTHjFvja3GlxQ";
+export const audience = "https://api.nutrition-tracker.com";
